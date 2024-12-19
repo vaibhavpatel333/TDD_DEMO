19efde2eff191735209be7f99f897774778381a7
nikunjvagadiyass-sdei <nikunjvagadiya.ss@smartdatainc.net>
2024-12-19 11:37:44 +0530

    add test case for a single number

--- test/stringCalculator.test.mjs
+++ test/stringCalculator.test.mjs
@@ -5,4 +5,9 @@ describe('String Calculator', () => {
     it('returns 0 for an empty string', () => {
       expect(add('')).to.equal(0);
     });
+
+    it('returns the number for a single number', () => {
+        expect(add('1')).to.equal(1);
+    });
+
   });

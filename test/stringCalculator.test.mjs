7c85e8b683857c5cfc4d3bd4a57fb612775a7ca7
nikunjvagadiyass-sdei <nikunjvagadiya.ss@smartdatainc.net>
2024-12-19 11:45:59 +0530

    add test case for a multiple numbers

--- test/stringCalculator.test.mjs
+++ test/stringCalculator.test.mjs
@@ -1,13 +1,16 @@
-import { expect } from 'chai';
-import { add } from '../stringCalculator.js'
+import { expect } from "chai";
+import { add } from "../stringCalculator.js";
 
-describe('String Calculator', () => {
-    it('returns 0 for an empty string', () => {
-      expect(add('')).to.equal(0);
-    });
-
-    it('returns the number for a single number', () => {
-        expect(add('1')).to.equal(1);
-    });
-
+describe("String Calculator", () => {
+  it("returns 0 for an empty string", () => {
+    expect(add("")).to.equal(0);
   });
+
+  it("returns the number for a single number", () => {
+    expect(add("1")).to.equal(1);
+  });
+
+  it("returns the sum of two numbers", () => {
+    expect(add("1,5")).to.equal(6);
+  });
+});

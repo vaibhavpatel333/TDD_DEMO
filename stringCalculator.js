93b886f013edc1927021ac346ebb09cadb3bf72c
nikunjvagadiyass-sdei <nikunjvagadiya.ss@smartdatainc.net>
2024-12-19 11:48:23 +0530

    implement handle a single number and sum functionality for two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,3 +1,13 @@
 export function add(numbers) {
     if (numbers === '') return 0;
+
+    let delimiter = /[,\n]/;
+    let numbersToAdd = numbers;
+
+    const nums = numbersToAdd.split(delimiter)
+    .map(str => str.trim())
+    .filter(str => str !== '')
+    .map(Number);
+
+    return nums.reduce((sum, num) => sum + num, 0);
 }

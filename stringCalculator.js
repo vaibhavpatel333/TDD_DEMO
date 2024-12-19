5d8cd4a3edd8b36bfccf477cfbc29c9e6f5d3aa2
nikunjvagadiyass-sdei <nikunjvagadiya.ss@smartdatainc.net>
2024-12-19 11:35:40 +0530

    implement add function to handle empty strings

---
+++ stringCalculator.js
@@ -0,0 +1,3 @@
+export function add(numbers) {
+    if (numbers === '') return 0;
+}
--- test/stringCalculator.test.mjs
+++ test/stringCalculator.test.mjs
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { add } from '../stringCalculator.js'
 
 describe('String Calculator', () => {
     it('returns 0 for an empty string', () => {
